5eccec99661da1a2a2416c93f249d81260d07305
JIrleidySantos <jirleidysantosm@gmail.com>
2025-02-07 15:29:30 -0500

    Crear mmi rama

---
+++ src/main/java/ec/edu/espoch/mvcpractica07/controlador/Controlador.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espoch.mvcpractica07.controlador;
+
+/**
+ *
+ * @author mundo
+ */
+public class Controlador {
+    
+}
